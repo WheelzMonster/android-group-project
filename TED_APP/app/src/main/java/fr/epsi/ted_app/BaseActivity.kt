1c56cfc0862767ca49a6ccef1ec9e90e07472753
Naylink-x <58996827+Naylink-x@users.noreply.github.com>
2021-12-09 12:06:58 +0100

    Add BaseActivity & layout_header

---
+++ TED_APP/app/src/main/java/fr/epsi/ted_app/BaseActivity.kt
@@ -0,0 +1,21 @@
+package fr.epsi.ted_app
+
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+open class BaseActivity: AppCompatActivity(){
+    fun showBack(){
+        val imageViewBack = findViewById<ImageView>(R.id.imageViewBack)
+        imageViewBack.visibility= View.VISIBLE
+        imageViewBack.setOnClickListener(View.OnClickListener {
+            onBackPressed()
+        })
+    }
+
+    fun setHeaderTitle(text:String){
+        val textViewTitle = findViewById<TextView>(R.id.textViewTitle)
+        textViewTitle.text=text
+    }
+}

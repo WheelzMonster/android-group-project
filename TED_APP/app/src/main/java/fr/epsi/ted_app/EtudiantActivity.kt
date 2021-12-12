3daafb09f7c8ad87cf86c9dfb16a5592668949e4
ththieul <tibo.thieuleux@gmail.com>
2021-12-12 17:57:01 +0100

    modified some color and optimised code

--- TED_APP/app/src/main/java/fr/epsi/ted_app/EtudiantActivity.kt
+++ TED_APP/app/src/main/java/fr/epsi/ted_app/EtudiantActivity.kt
@@ -10,6 +10,7 @@ class EtudiantActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_etudiant)
         setHeaderTitle("Infos")
+        showBack()
 
         val buttonLouis = findViewById<Button>(R.id.button_etudiant_louis)
         val buttonRomain = findViewById<Button>(R.id.button_etudiant_romain)

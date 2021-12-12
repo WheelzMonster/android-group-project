12aa8e4db1c681806f0c8602b149e166fecbabd5
ththieul <tibo.thieuleux@gmail.com>
2021-12-12 15:59:23 +0100

    modified main page and added the beginning of student page

--- TED_APP/app/src/main/java/fr/epsi/ted_app/MainActivity.kt
+++ TED_APP/app/src/main/java/fr/epsi/ted_app/MainActivity.kt
@@ -2,10 +2,20 @@ package fr.epsi.ted_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.content.Intent
 
 class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setHeaderTitle("Epsi")
+        val buttonEtudiant = findViewById<Button>(R.id.button_home_etudiants)
+
+        buttonEtudiant.setOnClickListener(View.OnClickListener {
+            val newIntent = Intent(application,EtudiantActivity::class.java)
+            startActivity(newIntent)
+        })
     }
 }

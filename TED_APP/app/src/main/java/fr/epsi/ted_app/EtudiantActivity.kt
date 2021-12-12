d33e257a539e5fba8665a4aede656990be39252e
ththieul <tibo.thieuleux@gmail.com>
2021-12-12 17:10:59 +0100

    modified etudiant page and added different styles to buttons

--- TED_APP/app/src/main/java/fr/epsi/ted_app/EtudiantActivity.kt
+++ TED_APP/app/src/main/java/fr/epsi/ted_app/EtudiantActivity.kt
@@ -2,10 +2,17 @@ package fr.epsi.ted_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class EtudiantActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_etudiant)
+        setHeaderTitle("Infos")
+
+        val buttonLouis = findViewById<Button>(R.id.button_etudiant_louis)
+        val buttonRomain = findViewById<Button>(R.id.button_etudiant_romain)
+        val buttonThibault = findViewById<Button>(R.id.button_etudiant_thibault)
     }
 }

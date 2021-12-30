ec6336c00b6f6a972ed3d5328b4394e831154219
ththieul <tibo.thieuleux@gmail.com>
2021-12-31 00:01:21 +0100

    added product big desc part

---
+++ TED_APP/app/src/main/java/fr/epsi/ted_app/ProductDetailActivity.kt
@@ -0,0 +1,27 @@
+package fr.epsi.ted_app
+
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import com.squareup.picasso.Picasso
+
+class ProductDetailActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_product_detail)
+        intent.getStringExtra("title")?.let { setHeaderTitle(it)}
+        showBack()
+
+        val descriptionProduct = intent.getStringExtra("description")
+        val pictureProduct = intent.getStringExtra("url")
+
+        var descriptionTextView = findViewById<TextView>(R.id.textViewProductDetail)
+        val imageViewProduct = findViewById<ImageView>(R.id.imageViewProductDetail)
+
+        descriptionTextView.text = descriptionProduct
+        Picasso.get().load(pictureProduct).into(imageViewProduct)
+
+
+
+    }
+}
